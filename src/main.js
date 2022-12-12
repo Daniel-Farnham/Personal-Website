f9bc5829dc08b60f1700d3c0cfd92518643a4f56
D.Farnham <danjf1210@gmail.com>
2022-12-12 12:47:56 +1100

    updated nav bar

--- src/main.js
+++ src/main.js
@@ -6,94 +6,48 @@ const strText = text.textContent;
 const splitText = strText.split(""); 
 text.textContent = ""; 
 
+const my_name = document.getElementsByClassName("name");
 
-
-
-console.log(strText); 
-console.log(splitText); 
-
-
-
+const nav_bar = document.querySelector("ul"); 
 
 for (let i=0; i < splitText.length; i++) {
-    text.innerHTML += "<span>" + splitText[i] + "</span>";
-    console.log(splitText[i]); 
+    text.innerHTML += "<span>" + splitText[i] + "</span>";; 
     
 }
-
-/* const changeColour = (a) => {
-    a.classList.add('NewColour'); 
-}
-
-let = NewDaniel = changeColour(document.querySelector("Daniel")) 
-
-
-console.log(NewDaniel); 
-*/
 
 let char = 0;
 let timer = setInterval(onTick, 125)
-// the timer selects calls onTick and repeats it every 'x' milliseconds. 
 
-// the function onTick, selects all of spans and the [char] ensures we are selecting each individual one. 
-//It takes every span. 
-// adds in the class list 'fade'. 
-//Once we have the class list 'fade we can 
 function onTick() {
     const span = text.querySelectorAll('span')[char]; 
     span.classList.add('fade'); // adding in new class 
-    
-    /* if (char > 20) {
-        span.classList.add('fade', 'newColour')
-    }
-    else {
-        span.classList.add('fade'); 
-    }
-
-    let blueElement = document.getElementsByClassName('newColour');  This is just some leftover code to help change my name to blue */ 
 
     char++ 
     
-    if (char === splitText.length) {
 
+    if (char === splitText.length) {
+        addNav(); 
         complete();
+        //nameColor(); 
         return; 
-        
     }
 }
+
+function addNav() {
+    nav_bar.style.opacity = 1;  
+   
+}
+
+function nameColor() {
+    // my_name.style.color = "blue"; 
+}
+
 
 function complete () {
     clearInterval(timer); 
     timer = null; 
 }
 
-// it will be cool to just change my name to blue. 
-/*
 
 
 
-/* Section for animated scroll 
-
-const intro = document.querySelector('.Playground-Vintage'); 
-const video = intro.querySelector('video');
-const aboutText = intro.querySelector('h1');
-
-// End Section
-
-const section = document.querySelector('section');
-const end = section.querySelector('h1'); 
-
-// Scroll Magic
-
-const controller = new ScrollMagic.Controller(); 
-
-// Scene handles blocks of animations 
-const scene = new ScrollMagic.Scene({
-    duration: 1000,
-    triggerElement: intro,
-    triggerHook: 0
-})
-.addIndicators()
-.addTo(controller); 
-
-*/
